fdbf40b4794d65e74bf0929c4065eab08a2e1164
cokia <hwymaster01@gmail.com>
2019-09-15 01:08:19 +0900

    asd

--- index.js
+++ index.js
@@ -16,17 +16,14 @@ app.use(express.json());
 var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', () => console.log(fs.readFileSync('mongo.txt').toString()));
-mongoose.connect('mongodb://docker.cloudus.io:32770/notatest', {useNewUrlParser: true});
+mongoose.connect('mongodb://docker.cloudus.io:32770/mongodb_tutorial', {useNewUrlParser: true});
 web3.setProvider(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/3c6820e798874f8ab12d8032821973de'));
 var abi = [{"constant": false, "inputs": [{ "name": "spender", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "from", "type": "address" }, { "name": "to", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "who", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "to", "type": "address" }, { "name": "value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "owner", "type": "address" }, { "name": "spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }];
 var contractAddress = "0x05e12955059a98b0da00cdb26cbea62e766cc0e8";
 var tokencontract = new web3.eth.Contract(abi,contractAddress)
 var transfertoken = function sendeth(privatekey,walletaddr,toaddr,value) { var rawTransaction = {"from": walletaddr,"nonce": web3.toHex(count),"gasPrice": "0x04e3b29200","gasLimit": "0x7458","to": contractAddress,"value": "0x0","data": contract.transfer.getData(toaddr, value, {from: walletaddr}),"chainId": 0x03};var privKey = new Buffer(privatekey, 'hex');var tx = new Tx(rawTransaction); tx.sign(privKey); var serializedTx = tx.serialize(); web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) { if (!err) console.log(hash);else console.log(err);});};
-var checklogin = function checklogin(){if(!req.session && req.session.logined) {return "login" }}
-var isAuth = (req, res, next) => {if(!req.session && req.session.logined) {return next();}};
 var UserSchema = new Schema({company: String,id: String,pw: String,type: String,name: String,email: String,phone: String,wallet_addr: String,status: String,wallet_privkey: String,description: String,userwhere:String});
-var userPaperSchema = new Schema({hash: String,ownerid: String,address: String,data: String,paper: String,memo: String,id: String,confirmed: String});
-var eduUserSchema = new Schema({id: String,name: String,paper: String,owner: String,address: String,opendate: String,phone: String,pw: String,confirmed: Boolean})
+var userPaperSchema=new Schema({hash:String,confirmed:Boolean,ownerid:String,status:String,koreanname:String,englishname:String,birthdate:String,mail:String,phone:String,address:String,school1:{school1_name:String,school1_graduate:String,school1_where:String,school1_graduatedate:String},school2:{school2_name:String,school2_graduate:String,school2_where:String,school2_graduatedate:String},school3:{school3_name:String,school3_graduate:String,school3_major:String,school1_where:String,school1_graduatedate:String},work1:{work1_date:String,work1_name:String,work1_position:String,work1_majorwork:String},work2:{work2_date:String,work2_name:String,work2_position:String,work2_majorwork:String},work3:{work3_date:String,work3_name:String,work3_position:String,work3_majorwork:String},army:Boolean,description:String});var eduUserSchema = new Schema({id: String,name: String,paper: String,owner: String,address: String,opendate: String,phone: String,pw: String,confirmed: Boolean})
 var tempSchema = new Schema({dapptransaction: "String"});
 var classdataSchema = new Schema({eduName:String,description:String,teacher:String, organizer:String,status:String});
 var joinSchema = new Schema({userid:String,classname:String,classid:String,classorganizer:String});
@@ -101,8 +98,10 @@ app.post('/login', function(req, res) {
     	
 // });
 app.get('/admin1', function(req, res){
-    eduUser.find({confirmed: false },{_id:1 ,id: 1,name: 1,paper: 1,owner: 1,address: 1,opendate: 1,phone: 1,pw: 0},(function(err, users){
+    eduUser.find({confirmed: false}, { pw: 0 , __v:0}, (function(err, users){
+        console.log(err);
         if(err) return res.status(500).send({error: 'database failure'});
+        if(!eduUser) return res.status(404).json({ error: 'user not found' });
         res.json(users);	
 }));
 });
@@ -118,21 +117,21 @@ app.post('/admin1/accept', function(req,res){
     });
 });
     app.post('/admin1/denied', function(req,res){
-    eduUser.remove({ id: req.query.id }, function(err, output){
+    eduUser.remove({ _id: req.query.id }, function(err, output){
         if(err) return res.status(500).json({ error: "database failure" });
-        if(!output.result.n) return res.status(404).json({ error: "user not found" });
+        // if(!output.result.n) return res.status(404).json({ error: "user not found" });
         res.json({result: "ok"});
         res.status(204).end();
     })
 });
 
 app.get('/admin2', function(req, res){
-    UserPaper.find({confirmed: false },{id_:1,owner: 1,address: 1,hash: 1, data: 1,paper: 1,memo: 1,id: 1,confirmed: 0},(function(err, userPaper){
+    UserPaper.find({confirmed: false },{id_:0, confirmed: 0},(function(err, userPaper){
         if(err) return res.status(500).send({error: 'database failure'});
         res.json(userPaper);	
 }));
 });
-app.post('/admin2/accpet', function(req, res){
+app.post('/admin2/accept', function(req, res){
     UserPaper.findById(req.query.id, function(err, UserPaper){
         if(err) return res.status(500).json({ error: 'database failure' });
         if(!UserPaper) return res.status(404).json({ error: 'userpaper is not found' });
@@ -166,7 +165,7 @@ app.get('/admin3', function(req,res){
 });
 
 app.post('/admin3/new', function(req,res){
-req.query.
+//NEW DAPP
 });
 
 app.get('/admin4', function(req, res){
@@ -262,9 +261,80 @@ app.get('/main3/:classid', function(req,res){
 });
 
 app.get('/edu1', function(req,res){
+
     //
 })
+// ------------temp------------
+app.post('/temp1', function(req,res){
+    console.log("temp1")
+     var eduuser = new eduUser();
+            eduuser.id=req.query.id,
+            eduuser.name=req.query.name,
+            eduuser.paper=req.query.paper,
+            eduuser.owner=req.query.owner,
+            eduuser.address=req.query.address,
+            eduuser.opendate=req.query.opendate,
+            eduuser.phone=req.query.phone,
+            eduuser.pw=req.query.pw,
+            eduuser.confirmed=false,
+        eduuser.save(function(err){
+            if(err){
+                console.error(err);
+                res.json({result: "error"});
+                return res.json({result: "ok"});
+        };
+        });
+})
 
+app.post('/temp2', function(req,res){
+    console.log('temp2')
+    var paper = new UserPaper();
+    paper.hash= req.query.hash,
+    paper.confirmed = req.query.confirmed,
+    paper.ownerid= req.query.ownerid,
+    paper.status=req.query.status,
+    paper.koreanname= req.query.koreanname,
+    paper.englishname=req.query.englishname,
+    paper.birthdate= req.query.birthdate,
+    paper.mail= req.query.mail,
+    paper.phone= req.query.phone,
+    paper.address= req.query.address,
+    paper.school1.school1_name= req.query.school1_name,
+    paper.school1.school1_graduate= req.query.school1_graduate,
+    paper.school1.school1_where= req.query.school1_where,
+    paper.school1.school1_graduatedate= req.query.school1_graduatedate,
+    paper.school2.school2_name= req.query.school2_name,
+    paper.school2.school2_graduate= req.query.school2_graduate,
+    paper.school2.school2_where= req.query.school2_where,
+    paper.school2.school2_graduatedate= req.query.school1_graduatedate,
+    paper.school3.school3_name=req.query.school3_name,
+    paper.school3.school3_graduate=req.query.school3_graduate,
+    paper.school3.school3_major= req.query.school3_major,
+    paper.school3.school3_where=req.query.school3_where,
+    paper.school3.school3_graduatedate=req.query.school3_graduatedate,
+    paper.work1.work1_date=req.query.work1_date,
+    paper.work1.work1_name=req.query.work1_name,
+    paper.work1.work1_position=req.query.work1_position,
+    paper.work1.work1_majorwork=req.query.work1_majorwork,
+    paper.work2.work2_date=req.query.work2_date,
+    paper.work2.work2_name=req.query.work2_name,
+    paper.work2.work2_position=req.query.work2_position,
+    paper.work2.work2_majorwork=req.query.work2_majorwork,
+    paper.work3.work3_date=req.query.work3_date,
+    paper.work3.work3_name=req.query.work3_name,
+    paper.work3.work3_position=req.query.work3_position,
+    paper.work3.work3_majorwork=req.query.work3_majorwork,
+    paper.army=req.query.army,
+    paper.description=req.query.description,
+    paper.save(function(err){
+        if(err){
+            console.error(err);
+            res.json({result: "error"});
+            return res.json({result: "ok"});
+    };
+    });
+})
 app.listen(3000, function () {
     console.log("하와와!");
+    
 });
