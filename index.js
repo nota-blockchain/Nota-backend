725a773fd8d7a3a56c18b5015cc30e66245364fe
cokia <hwymaster01@gmail.com>
2019-09-16 06:04:36 +0900

    Asd

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var contractAddress = "0x05e12955059a98b0da00cdb26cbea62e766cc0e8";
 var tokencontract = new web3.eth.Contract(abi,contractAddress)
 var transfertoken = function sendeth(privatekey,walletaddr,toaddr,value) { var rawTransaction = {"from": walletaddr,"nonce": web3.toHex(count),"gasPrice": "0x04e3b29200","gasLimit": "0x7458","to": contractAddress,"value": "0x0","data": contract.transfer.getData(toaddr, value, {from: walletaddr}),"chainId": 0x03};var privKey = new Buffer(privatekey, 'hex');var tx = new Tx(rawTransaction); tx.sign(privKey); var serializedTx = tx.serialize(); web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) { if (!err) console.log(hash);else console.log(err);});};
 var UserSchema = new Schema({company: String,id: String,pw: String,type: String,name: String,email: String,phone: String,wallet_addr: String,status: String,wallet_privkey: String,description: String,userwhere:String});
-var userPaperSchema=new Schema({hash:String,confirmed:Boolean,ownerid:String,status:String,koreanname:String,englishname:String,birthdate:String,mail:String,phone:String,address:String,school1:{school1_name:String,school1_graduate:String,school1_where:String,school1_graduatedate:String},school2:{school2_name:String,school2_graduate:String,school2_where:String,school2_graduatedate:String},school3:{school3_name:String,school3_graduate:String,school3_major:String,school1_where:String,school1_graduatedate:String},work1:{work1_date:String,work1_name:String,work1_position:String,work1_majorwork:String},work2:{work2_date:String,work2_name:String,work2_position:String,work2_majorwork:String},work3:{work3_date:String,work3_name:String,work3_position:String,work3_majorwork:String},army:Boolean,description:String});var eduUserSchema = new Schema({id: String,name: String,paper: String,owner: String,address: String,opendate: String,phone: String,pw: String,confirmed: Boolean})
+var userPaperSchema = new Schema({hash:String,confirmed:Boolean,ownerid:String,sex:String,koreanname:String,englishname:String,birthdate:String,mail:String,phone:String,address:String,school1:{school1_name:String,school1_graduate:String,school1_where:String,school1_graduatedate:String},school2:{school2_name:String,school2_graduate:String,school2_where:String,school2_graduatedate:String},school3:{school3_name:String,school3_graduate:String,school3_where:String,school3_graduatedate:String},school4:{school4_name:String,school4_graduate:String,school4_major:String,school4_where:String,school4_graduatedate:String},work1:{work1_date:String,work1_name:String,work1_position:String,work1_majorwork:String},work2:{work2_date:String,work2_name:String,work2_position:String,work2_majorwork:String},work3:{work3_date:String,work3_name:String,work3_position:String,work3_majorwork:String},army:Boolean,description:String});var eduUserSchema=new Schema({id:String,name:String,paper:String,owner:String,address:String,opendate:String,phone:String,pw:String,confirmed:Boolean});
 var tempSchema = new Schema({dapptransaction: "String"});
 var classdataSchema = new Schema({eduName:String,description:String,teacher:String, organizer:String,status:String});
 var joinSchema = new Schema({userid:String,classname:String,classid:String,classorganizer:String});
@@ -160,9 +160,6 @@ app.get('/admin3/<txid>', function(req,res){
     //dapp result print
     });
 
-app.get('/admin3', function(req,res){
-//dapp result print
-});
 
 app.post('/admin3/new', function(req,res){
 //NEW DAPP
@@ -200,24 +197,48 @@ app.post('/admin4/3', function(req, res){
 
 //-------------main----------------
 app.post('/main1', function(req,res){
-    var paper = new userPaper();
-    paper.hash=req.body.hash,
-    paper.ownerid=req.body.ownerid,
-    paper.status=req.body.status,
-    paper.koreanname=req.body.koreanname,
-    paper.englishname=req.body.englishname,
-    paper.birthdate=req.body.birthdate,
-    paper.mail=req.body.mail,
-    paper.phone=req.body.phone,
-    paper.address=req.body.address,
-    paper.school1=req.body.school1,
-    paper.school2=req.body.school2,
-    paper.school3=req.body.school3,
-    paper.work1=req.body.work1,
-    paper.work2=req.body.work2,
-    paper.work3=req.body.work3,
-    paper.army=req.body.army,
-    paper.description=req.body.description,
+    var paper = new UserPaper();
+    paper.hash= req.query.hash,
+    paper.confirmed = false,
+    paper.ownerid= req.query.ownerid,
+    paper.sex=req.query.sex,
+    paper.koreanname= req.query.koreanname,
+    paper.englishname=req.query.englishname,
+    paper.birthdate= req.query.birthdate,
+    paper.mail= req.query.mail,
+    paper.phone= req.query.phone,
+    paper.address= req.query.address,
+    paper.school1.school1_name= req.query.school1_name,
+    paper.school1.school1_graduate= req.query.school1_graduate,
+    paper.school1.school1_where= req.query.school1_where,
+    paper.school1.school1_graduatedate= req.query.school1_graduatedate,
+    paper.school2.school2_name= req.query.school2_name,
+    paper.school2.school2_graduate= req.query.school2_graduate,
+    paper.school2.school2_where= req.query.school2_where,
+    paper.school2.school2_graduatedate= req.query.school1_graduatedate,
+    paper.school3.school3_name=req.query.school3_name,
+    paper.school3.school3_graduate=req.query.school3_graduate,
+    paper.school3.school3_where=req.query.school3_where,
+    paper.school3.school3_graduatedate=req.query.school3_graduatedate,
+    paper.school4.school4_name=req.query.school4_name,
+    paper.school4.school4_graduate=req.query.school4_graduate,
+    paper.school4.school4_major= req.query.school4_major,
+    paper.school4.school4_where=req.query.school4_where,
+    paper.school4.school4_graduatedate=req.query.school4_graduatedate,
+    paper.work1.work1_date=req.query.work1_date,
+    paper.work1.work1_name=req.query.work1_name,
+    paper.work1.work1_position=req.query.work1_position,
+    paper.work1.work1_majorwork=req.query.work1_majorwork,
+    paper.work2.work2_date=req.query.work2_date,
+    paper.work2.work2_name=req.query.work2_name,
+    paper.work2.work2_position=req.query.work2_position,
+    paper.work2.work2_majorwork=req.query.work2_majorwork,
+    paper.work3.work3_date=req.query.work3_date,
+    paper.work3.work3_name=req.query.work3_name,
+    paper.work3.work3_position=req.query.work3_position,
+    paper.work3.work3_majorwork=req.query.work3_majorwork,
+    paper.army=req.query.army,
+    paper.description=req.query.description,
     paper.save(function(err){
         if(err){
             console.error(err);
@@ -225,10 +246,11 @@ app.post('/main1', function(req,res){
             return res.json({result: "ok"});
     };
     });
+    res.status(204).end();
 })
 
 app.get('/main1', function(req,res){
-    User.find({id: req.query.id}, {company: 1,id: 0,pw: 0,type: 0,name: 1,email: 1,phone: 1,wallet_addr: 0,status: 0,wallet_privkey: 0,description: 0,userwhere: 1},  function(err, users){
+    User.find({id: req.query.id}, {_id: 0, __v:0,pw: 0,type: 0,wallet_addr: 0,status: 0,wallet_privkey: 0,description: 0},  function(err, users){
         if(err) return res.status(500).json({error: err});
         if(users.length === 0) return res.status(404).json({error: 'User not found'});
         res.json(users);
